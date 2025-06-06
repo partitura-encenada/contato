f149c04896ed065c500e10caacd5c2b9011cf278
c-breder <c.brederfigueiredo@gmail.com>
2025-06-06 17:42:44 -0300

    Correção completa do loopmidi

--- espetaculo_part1/genesis2/mao_esquerda.py
+++ espetaculo_part1/genesis2/mao_esquerda.py
@@ -13,7 +13,7 @@ serialString = ''
 
 midiout = rtmidi.MidiOut()
 print(midiout.get_ports())
-port = midiout.open_port(4)
+port = midiout.open_port(5)
 
 with open('mapNotas.json') as jsonfile:
       mapNotas = json.load(jsonfile)
